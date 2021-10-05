98d315ed8b8200ac6b3b09400569ed2ae078fbfd
Vágner Dávid <71004491+vagnerdavid@users.noreply.github.com>
2021-10-05 19:12:28 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import string
+
+def strip_punctuations(line):
+    for character in string.punctuation:
+        line = line.replace(character, "")
+    return line
+
+filepath = "../szavakGyakorisagaPython/bee.txt"
+
+word_count = {}
+
+with open(filepath, 'r', encoding='utf-8') as fi:
+    for line in fi:
+        line = strip_punctuations(line)
+        words = line.split()
+
+        for word in words:
+            word = word.lower()
+            if word not in word_count:
+                word_count[word] = 0
+            word_count[word] += 1
+
+list(word_count.keys())[:10]
+
+ten_words = list(word_count.keys())[:10]
+for word in ten_words:
+    print("{0:15}{1:8d}".format(word, word_count[word]))
+
+def order_dict_by_freq(dictionary):
+    sorted_values = []
+    for key in dictionary:
+        sorted_values.append((dictionary[key], key))
+    sorted_values = sorted(sorted_values)
+    sorted_values = sorted_values[::-1]
+    return sorted_values
+
+top_words = order_dict_by_freq(word_count)[:10]
+for tuple_freq in top_words:
+    count, word = tuple_freq
+    print("{0:15}{1:8d}".format(word, count))
