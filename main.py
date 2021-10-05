ff62409172bf2c5025a4c8da7a59c6890067e243
Vágner Dávid <71004491+vagnerdavid@users.noreply.github.com>
2021-10-05 20:57:06 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import string
 
+
 def strip_punctuations(line):
     for character in string.punctuation:
         line = line.replace(character, "")
     return line
 
-filepath = "../szavakGyakorisagaPython/bee.txt"
 
+filepath = "../szavakGyakorisagaPython/EgriCsillagok.txt"
+zCount = 0
 word_count = {}
 
 with open(filepath, 'r', encoding='utf-8') as fi:
@@ -16,15 +18,17 @@ with open(filepath, 'r', encoding='utf-8') as fi:
 
         for word in words:
             word = word.lower()
+
             if word not in word_count:
                 word_count[word] = 0
             word_count[word] += 1
 
 list(word_count.keys())[:10]
 
-ten_words = list(word_count.keys())[:10]
+ten_words = list(word_count.keys())[:0]
 for word in ten_words:
     print("{0:15}{1:8d}".format(word, word_count[word]))
+
 
 def order_dict_by_freq(dictionary):
     sorted_values = []
@@ -34,7 +38,11 @@ def order_dict_by_freq(dictionary):
     sorted_values = sorted_values[::-1]
     return sorted_values
 
-top_words = order_dict_by_freq(word_count)[:10]
+
+top_words = order_dict_by_freq(word_count)[:20]
 for tuple_freq in top_words:
     count, word = tuple_freq
     print("{0:15}{1:8d}".format(word, count))
+
+if word.charAt(0)=="z":
+    zCount +=1
